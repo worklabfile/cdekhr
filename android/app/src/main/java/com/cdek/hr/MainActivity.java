0bbd7da8acfdea7d8888e88279df754c2701e8f5
Vladimir Filippov <vladimirfilippov@MacBook-Air-Vladimir.local>
2025-06-07 14:53:54 +0300

    android apk

---
+++ android/app/src/main/java/com/cdek/hr/MainActivity.java
@@ -0,0 +1,5 @@
+package com.cdek.hr;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cdek.hr',
+  appName: 'cdekhr',
+  webDir: 'dist'
+};
+
+export default config;
